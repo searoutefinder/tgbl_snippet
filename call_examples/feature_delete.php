0c299c4a73360430d11229590ebf780fe10a5c8b
Tamas Hajdu <searoutefindercom@gmail.com>
2017-11-27 10:57:18 +0100

    Update feature_delete.php

--- call_examples/feature_delete.php
+++ call_examples/feature_delete.php
@@ -15,6 +15,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "DELETE",
+  CURLOPT_SSL_VERIFYPEER => false,
   CURLOPT_HTTPHEADER => array(
     "cache-control: no-cache"
   ),
