f74c0af2dd0cc79431424b3d0e218a24fed6fed2
Tamas Hajdu <searoutefindercom@gmail.com>
2017-11-27 11:04:53 +0100

    Get single feature by its ID

---
+++ call_examples/get_single_feature.php
@@ -0,0 +1,36 @@
+<?php
+
+$curl = curl_init();
+
+$user = "rperry";
+$dataset = "cj9ebj0o4114d32o78msvxynv";
+$accessToken = "sk.eyJ1IjoicnBlcnJ5IiwiYSI6ImNqYWUyN3U5aTFyYnAzM3IxdnIyaWE2eTEifQ.f9MMdw138ZVqirYZDJBVwQ";
+$featureID = "feature-ID-synced-with-magento-record";
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.mapbox.com/datasets/v1/" . $user . "/" . $dataset . "/features/" . $featureID . "?access_token=" . $accessToken,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_SSL_VERIFYPEER => false, 
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer 54iqyv3abua1ys15qh2y3zwtpl",
+    "cache-control: no-cache",
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+?>
